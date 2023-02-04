109621a8b84d1f5c1a6591ca6c307c9fee5dadb4
HilltopWorks <115959038+HilltopWorks@users.noreply.github.com>
2023-02-04 15:18:54 -0800

    Horizontal Font working

--- scps_150.26.asm
+++ scps_150.26.asm
@@ -1,4 +1,4 @@
-.psx
+.ps2
 .erroronwarning on
 
 
@@ -28,92 +28,101 @@
 .definelabel 	draw_char_7, 0x00180670
 .definelabel	draw_char, 0x00180680 ; int x, int y, uint char, B(7?)
 .definelabel	stash_char, 0x001fde94
-.definelabel	draw_dialogue_char_start, 0x001fdeac ;start of char drawing routine in main dialogue
+.definelabel	draw_dialogue_char_start, 0x001fdeac ;start of char drawing routine in dialogue
 .definelabel	draw_dialogue_char_end, 0x1fdf54
 .definelabel	set_font_color, 0x180168	;Br Bg Bb Ba
-.definelabel	set_x_top, 0x1fde34
+.definelabel 	set_spacing, 0x1fde94
+.definelabel	set_kerning, 0x1fdf58
+.definelabel	set_x_top,   0x1fde34
 .definelabel	set_x_top_2, 0x001fdd40
+.definelabel	set_y_base, 0x1fde98
+.definelabel 	get_fb_y, 0x0012D3C8
+.definelabel 	get_fb_x, 0x0012D390
 
 .definelabel	font_color, 0x2615c8
 .definelabel	font_color_u, 0x26
 .definelabel	font_color_l, 0x15c8
 
-current_x equ  s4
+curr_x equ  s4
 newlines equ v1
 gpu_call equ s0
 
-base_x equ 0x50
-base_y equ 0x150
-font_width equ 0x17
-font_spacing equ 0x19
-white equ 0x80FFFFFF
-;Free: v0, a3, a2, a1, s2
-
-;x left = current_x               ;x right = current_x + 0x17
-;y top = base_y + (newlines*0x1A) ;y bot = base_y + (newlines*0x1A) + 0x17
-;char = 
+base_x equ 0x330
+base_y equ 0xAD
+font_width equ 0xd0	 ; kerning
+font_spacing equ 0xC ; newline spacing
+white  equ 0x80FFFFFF
+black  equ 0x80000000
+yellow equ 0x8000FFFF
 
 .org draw_dialogue_char_start
 .area draw_dialogue_char_end - draw_dialogue_char_start,0
 
 	;Set main font color
-	la v0, white
-	sw v0, 0x0(gpu_call)
+	la a1, white
+	sw a1, 0x0(gpu_call)
 	
-	;autopilot    		; a2 = base_x, 
+	;autopilot    		; a2 = base_y, 
 	mfhi	a3 			; a3 = char%17
 	mflo	a1 			; a1 = char/17
-	mult	v0,v1,v0 	; v0 = newlines*spacing
-	mult	a1,a1,s7 	; a1 = (char/17)*0x16, aka glyph height x row aka v
-	li		v1,0x80
-	mult	zero,a3,s7 	; lo = (char%0x17)*0x16, aka glyph width x column aka u
-	sb		v1,0x3(s0)
-	subu	s2,a2,v0 	; s2 = base_x - (newlines*spacing)
+	mult	v0,v1,v0 	; v0 = newlines*spacing ;.word 0x00621018	
+	mult 	a1,a1,s7 	; a1 = (char/17)*0x16, aka glyph height x row aka v ;.word 0x00B72818	
+	addu	s2,a2,v0 	; s2 = base_y + (newlines*spacing)
+	mult	zero,a3,s7 	; lo = (char%0x17)*0x16, aka glyph width x column aka u ;.w 0x00F70018	
 	andi	a2,a1,0xFFFF; a2 = font row
 	mflo	a3			; a3 = u
-	addiu	a1,a2,0x17	; 
-	addiu	a0,a3,0x17
-	sll		a1,a1,0x04
-	sll		a0,a0,0x04
+	addiu	a1,a2,0x17	; a1 = v_bottom
+	addiu	a0,a3,0x17	; a0 = u_bottom
+	sll		a1,a1,0x04	; a1 = v_gpu_bottom
+	sll		a0,a0,0x04  ; a0 = u_gpu_bottom
+
+	sll	v0,a3,0x04		; a3 = u_gpu
+	sll	v1,a2,0x04		; a2 = v_gpu
+	sh	v0,0x8(gpu_call)		; STORE u_gpu
+	sh	v1,0xA(gpu_call)		; STORE v_gpu
+	sh	a0,0x18(gpu_call)		; STORE u_bottom_gpu
+	jal	get_fb_y				; v0 = frame_buffer_y
+	sh	a1,0x1A(gpu_call)		; STORE v_bottom_gpu	^
 	
-	;Set params
-	move a0, current_x ;x = current_x
-	
-	li v0, font_spacing;y = basey + (newlines*spacing)
-	mult v0, newlines
-	nop
-	nop
-	mflo v0
-	addi a1, v0, base_y
-	
-	;Store squashed registers
-	la v0, freespace
-	
-	sw t4, 0x0(v0)
-	sw t7, 0x4(v0)
-	sw v1, 0x8(v0)
 	
 
-	;Draw character
-	jal draw_char_7 ;squashes v0,v1,a0,a1,a2,t4,t7
-	nop
+	sll	v1,s2,0x04			; v1 = (base_y + (newlines*spacing)) *0x10 aka y_gpu unf
+	addu	v0,v1			; v0 = y_gpu unf + frame_buffer_y aka y_gpu
+	jal	get_fb_x			; v0 = frame_buffer_x
+	sh	v0,0x12(gpu_call)	; STORE y_gpu			^
+	addu	v0,curr_x		; v0 = frame_buffer_x + curr_x aka x_gpu
+	jal	get_fb_y			; v0 = frame_buffer_y
+	sh	v0,0x10(gpu_call)	; STORE x_gpu			^
 	
-	;Restore squashed registers
-	la v0, freespace
-	lw t4, 0x0(v0)
-	lw t7, 0x4(v0)
-	lw v1, 0x8(v0)
+	sll		s2,s2,0x04		; v1 = y_gpu << 4
+	addiu	v1,s2,0xB8		; v1 = y_gpu + 0xb8 aka y_gpu_bottom
 
-	;Increment text x position
-	addiu current_x, current_x, font_width
+
+	addu	v0,v1			; v0 = frame_buffer_y + y_bottom aka y_gpu_bottom
+	jal	get_fb_x			; v0 = frame_buffer_x
+	sh	v0,0x22(gpu_call)	; STORE y_gpu_bottom
+	addiu	v1,curr_x,0x170	; v1 = curr_x + 0xb8 aka x_right
+	addu	v0,v1			; v0 = x_right + frame_buffer_x aka x_gpu_right
+	sh	v0,0x20(gpu_call)	; STORE x_gpu_right
+	addiu	gpu_call,0x28	; gpu_cursor += 0x28
+	
 
 .endarea
 
+.org set_kerning
+	addiu s4, s4, font_width
+
+.org set_spacing
+	li v0, font_spacing
+
+.org set_y_base
+	li a2, base_y
+
 .org set_x_top
 	;Set base x after newline
-	li current_x, base_x
+	li curr_x, base_x
 .org set_x_top_2
 	;Set base x after newline
-	li current_x, base_x
+	li curr_x, base_x
 
 .close
